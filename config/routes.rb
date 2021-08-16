8ede65d00174224ad95152c788fb2f4a92f5510e
kobayashiryou <chiri.parihead@gmail.com>
2021-08-16 21:08:18 +0900

    add namespace in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :weights
-  mount_devise_token_auth_for "User", at: "auth"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, format: "json" do
+    namespace :v1 do
+      mount_devise_token_auth_for "User", at: "auth", controllers: {
+        registrations: "api/v1/auth/registrations",
+        sessions: "api/v1/auth/sessions",
+      }
+      resources :weights
+    end
+  end
 end
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
