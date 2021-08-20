701bee2bf5301d027dbe6c368cff8706805a1162
kobayashiryou <chiri.parihead@gmail.com>
2021-08-20 22:46:44 +0900

    create snack_spec

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
 RSpec.describe Snack, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation check" do
+    subject{ snack.valid? }
+    let!(:user){ create(:user) }
+    let!(:food){ create(:food) }
+    let(:snack){ build(:snack, date: date, user: user, food: food) }
+    let(:date){ Faker::Date.between(from: '2020-1-1', to: '2021-08-25') }
+    context "dateが入力されている場合" do
+      it "作成される" do
+        expect(subject).to eq true
+      end
+    end
+
+    context "dateが入力されていない時" do
+      let(:date){ nil }
+      it "エラーする" do
+        subject
+        expect(snack.errors.messages[:date]).to include "can't be blank"
+      end
+    end
+  end
 end
