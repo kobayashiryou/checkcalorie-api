312860a9278484873c7c2970f806499837dff092
kobayashiryou <chiri.parihead@gmail.com>
2021-08-20 22:44:45 +0900

    create breakfast_spec

--- spec/factories/breakfasts.rb
+++ spec/factories/breakfasts.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :breakfast do
-    user { nil }
-    food { nil }
+    date { Faker::Date.between(from: '2020-1-1', to: '2021-08-25') }
+    user
+    food
   end
 end
--- spec/models/breakfast_spec.rb
+++ spec/models/breakfast_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
 RSpec.describe Breakfast, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation check" do
+    subject{ breakfast.valid? }
+    let!(:user){ create(:user) }
+    let!(:food){ create(:food) }
+    let(:breakfast){ build(:breakfast, date: date, user: user, food: food) }
+    let(:date){ Faker::Date.between(from: '2020-1-1', to: '2021-08-25') }
+    context "dateが入力されている場合" do
+      it "作成される" do
+        expect(subject).to eq true
+      end
+    end
+
+    context "dateが入力されていない時" do
+      let(:date){ nil }
+      it "エラーする" do
+        subject
+        expect(breakfast.errors.messages[:date]).to include "can't be blank"
+      end
+    end
+  end
 end
