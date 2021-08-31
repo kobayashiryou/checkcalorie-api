0f2d5445786c015a7c843c3fe78cb592984dcb31
kobayashiryou <chiri.parihead@gmail.com>
2021-08-31 21:47:16 +0900

    execution rubocop -a

--- config/application.rb
+++ config/application.rb
@@ -35,10 +35,10 @@ module Checkcalorie
     config.middleware.use ActionDispatch::Flash
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins "http://localhost:3000"
         resource "*",
-        :headers => :any,
-        :methods => [:get, :post, :options, :delete, :put]
+                 headers: :any,
+                 methods: [:get, :post, :options, :delete, :put]
       end
     end
   end
