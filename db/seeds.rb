a32cd71d412e8421e703813310304133e95bf9a0
kobayashiryou <chiri.parihead@gmail.com>
2021-08-31 21:27:09 +0900

    default data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# users = [
+#   {email: "abc@example.com", password: "kobayasi"},
+#   {email: "def@example.com", password: "kobayasi"}
+# ]
+
+# User.create!(users)
+
+weights = [
+  {date: "2021-8-30", kg: 62, user_id: 1},
+  {date: "2021-8-31", kg: 63, user_id: 1}
+]
+
+Weight.create!(weights)
