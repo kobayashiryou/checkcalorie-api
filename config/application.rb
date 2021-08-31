2f3dfa74e9edd4e3271a86e4a03d5fe93f61c865
kobayashiryou <chiri.parihead@gmail.com>
2021-08-31 21:25:33 +0900

    add rack cors config

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,13 @@ module Checkcalorie
 
     config.api_only = true
     config.middleware.use ActionDispatch::Flash
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource "*",
+        :headers => :any,
+        :methods => [:get, :post, :options, :delete, :put]
+      end
+    end
   end
 end
