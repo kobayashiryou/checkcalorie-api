f7ad223d84d32ed9aa59d31eeaa043c22022c678
kobayashiryou <chiri.parihead@gmail.com>
2021-09-01 21:14:16 +0900

    use material-ui

---
+++ front_end/src/Components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+function ButtonFunction() {
+  return (
+    <Button variant="contained" color="secondary">
+      Primary
+    </Button>
+  );
+}
+
+export default ButtonFunction;
