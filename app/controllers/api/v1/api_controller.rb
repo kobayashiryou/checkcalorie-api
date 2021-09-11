f986d809f23d66ab777ac5e3f0eea70b20ded04e
kobayashiryou <chiri.parihead@gmail.com>
2021-09-11 22:07:19 +0900

    create api_controller

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::ApiController < ApplicationController
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  alias_method :current_user, :current_api_v1_user
+  alias_method :authenticate_user!, :authenticate_api_v1_user!
+  alias_method :user_signed_in?, :api_v1_user_signed_in?
+  def protect_from_forgery
+  end
+end
