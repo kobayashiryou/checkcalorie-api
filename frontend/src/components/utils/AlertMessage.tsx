446077a90dd47c924d234fc141b3051edf59cbcb
kobayashiryou <chiri.parihead@gmail.com>
2021-09-14 22:10:58 +0900

    add comment

--- frontend/src/components/utils/AlertMessage.tsx
+++ frontend/src/components/utils/AlertMessage.tsx
@@ -2,11 +2,13 @@ import React from "react"
 import Snackbar from "@material-ui/core/Snackbar"
 import MuiAlert, { AlertProps } from "@material-ui/lab/Alert"
 
+//refとは、idとかの属性値。相違点としては、Componentの中だけのことなので、影響を気にせず使える
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
   props,
   ref,
 ) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+  //...スプレッド構文
 })
 
 interface AlertMessageProps {
