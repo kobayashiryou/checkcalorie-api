b330d8cc0253535bffb22c2aea057ef4376ee17f
kobayashiryou <chiri.parihead@gmail.com>
2021-09-15 22:39:00 +0900

    execution rubocop-a

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::UsersController < Api::V1::ApiController
   def index
     if current_user
-      render json: { status: 200, current_user: current_user}
+      render json: { status: 200, current_user: current_user }
     else
-      render json: { status: 500, message: "ユーザーが存在しません"}
+      render json: { status: 500, message: "ユーザーが存在しません" }
     end
   end
 end
