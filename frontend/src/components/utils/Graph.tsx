fd0227e036039dd1f0acefb8cae5d094ee5d630d
kobayashiryou <chiri.parihead@gmail.com>
2021-09-17 21:34:47 +0900

    edit Graph.tsx

--- frontend/src/components/utils/Graph.tsx
+++ frontend/src/components/utils/Graph.tsx
@@ -1,28 +1,44 @@
 import React, {useState, useContext, useEffect} from "react"
 import Paper from '@material-ui/core/Paper';
 
+import "date-fns"
+import DateFnsUtils from "@date-io/date-fns"
+
 import {
   ArgumentAxis,
   ValueAxis,
   Chart,
   LineSeries,
 } from "@devexpress/dx-react-chart-material-ui";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker
+} from "@material-ui/pickers"
 
 import { getWeights } from "lib/api/weights"
-import { WeightData } from "interfaces";
+import { WeightData, WeightDate } from "interfaces";
 
 export const Graph = () => {
-
+  const selectDate = new Date();
   const [ weights, setWeights ] = useState<WeightData []>([])
+  const [ date, setDate ] = useState<Date | null>(selectDate)
 
   useEffect(() => {
     indexWeights();
   }, []);
 
+  const handleChange = (date: Date | null) => {
+    setDate(date);
+  }
+
   const indexWeights = async () => {
 
+    const monthDate: WeightDate = {
+      date: date,
+    }
+
     try {
-      const res = await getWeights ()
+      const res = await getWeights (monthDate)
       console.log(res)
 
       if(res?.status === 200) {
@@ -44,6 +60,18 @@ export const Graph = () => {
           <ValueAxis />
           <LineSeries valueField="kg" argumentField="date" />
         </Chart>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <KeyboardDatePicker
+            label="登録年月日"
+            format="MM/dd/yyyy"
+            id="date-picker-dialog"
+            value={date}
+            onChange={handleChange}
+            KeyboardButtonProps={{
+              "aria-label": "change date"
+            }}
+          />
+          </MuiPickersUtilsProvider>
       </Paper>
     </>
   )
