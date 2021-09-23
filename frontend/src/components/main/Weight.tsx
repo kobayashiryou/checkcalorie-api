b1ff2e03d77729adeb6254cfed5f07590596872e
kobayashiryou <chiri.parihead@gmail.com>
2021-09-23 22:34:40 +0900

    add update method

--- frontend/src/components/main/Weight.tsx
+++ frontend/src/components/main/Weight.tsx
@@ -17,16 +17,32 @@ import {
 } from "@devexpress/dx-react-chart-material-ui";
 import { ArgumentScale, ValueScale } from '@devexpress/dx-react-chart';
 import Paper from '@material-ui/core/Paper';
+import Modal from '@material-ui/core/Modal';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { Fade } from "@material-ui/core"
 
 
 
 
 import { selectWeight, WeightData } from "../../interfaces"
-import { createWeight, deleteWeight } from "../../lib/api/weights"
+import { createWeight, deleteWeight, updateWeight } from "../../lib/api/weights"
 import { getWeights } from "../../lib/api/weights"
-import { domainToASCII } from "url"
+import classes from "*.module.css"
+import { getDate } from "date-fns"
 
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 
 
@@ -37,7 +53,7 @@ export const Weight = () => {
   const [ kg, setKg ] = useState<string>("");
   const [ weights, setWeights ] = useState<selectWeight []>([])
   const [ loading, setLoading ] =useState<boolean>(true)
-
+  const [ open, setOpen ] = useState(false);
 
 
   const indexWeights = async () => {
@@ -45,10 +61,12 @@ export const Weight = () => {
     try {
 
       const res = await getWeights ();
-      console.log(res)
 
       if(res?.status === 200) {
-        setWeights(res.data)
+        setWeights([res.data].map((weight) => {
+          return {...weight}
+        }))
+        console.log(weights);
       }
     } catch(err) {
       console.log(err)
@@ -79,6 +97,35 @@ export const Weight = () => {
       console.log(err);
     }
   }
+
+  const updateSubmit = async (id: number, data: WeightData) => {
+    const selectData: selectWeight = {
+      id: id,
+      date: null,
+      kg: ""
+    }
+
+    const updateData: WeightData = {
+      date: date,
+      kg: kg
+    }
+
+    try {
+      const res = await updateWeight(selectData.id, updateData);
+      console.log(res);
+      indexWeights();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const handleOpen = () => {
+    setOpen(true);
+  }
+  const handleClose = () => {
+    setOpen(false);
+  }
+
 
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -139,13 +186,52 @@ export const Weight = () => {
             <ul>
               {
                 weights.map((weight) =>
-                <li key={ weight.id }>{weight.kg}
+                <li key={ weight.id }>{weight.date}<br />{weight.kg}
                   <Button
                     type="submit"
                     onClick={() => deleteSubmit(weight.id)}
                   >
                     削除
                   </Button>
+                  <Button
+                    onClick={handleOpen}
+                  >
+                    編集
+                  </Button>
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-describedby="modal-modal-description"
+                  >
+                    <Box sx={style}>
+                      <Typography id="modal-modal-description">
+                        <TextField
+                          variant="outlined"
+                          required
+                          label="体重"
+                          value={kg}
+                          onChange={event => setKg(event.target.value)}
+                        />
+                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                          <KeyboardDatePicker
+                            label="登録年月日"
+                            format="MM/dd/yyyy"
+                            id="date-picker-dialog"
+                            value={date}
+                            onChange={handleChange}
+                            KeyboardButtonProps={{
+                              "aria-label": "change date"
+                            }}
+                          />
+                          {/* <Button
+                            onClick={() => updateSubmit(weight.id)}
+                          >
+                            編集
+                          </Button> */}
+                        </MuiPickersUtilsProvider>
+                      </Typography>
+                    </Box>
+                  </Modal>
                 </li>
                 )
               }
